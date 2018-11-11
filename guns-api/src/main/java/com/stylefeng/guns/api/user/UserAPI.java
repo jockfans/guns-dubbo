6a05545008e9cb7cf1e745d456168567bd198061
BlackCat <wengyu188@163.com>
2018-11-11 12:24:39 +0800

    Ingore Url is complete

---
+++ guns-api/src/main/java/com/stylefeng/guns/api/user/UserAPI.java
@@ -0,0 +1,9 @@
+package com.stylefeng.guns.api.user;
+
+/**
+ * Created by misdeadgirl on 2018/11/8.
+ */
+public interface UserAPI {
+
+    public boolean userLogin(String username, String password);
+}
---
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/config/properties/JwtProperties.java
@@ -0,0 +1,84 @@
+package com.stylefeng.guns.rest.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * jwt相关配置
+ *
+ * @author fengshuonan
+ * @date 2017-08-23 9:23
+ */
+@Configuration
+@ConfigurationProperties(prefix = JwtProperties.JWT_PREFIX)
+public class JwtProperties {
+
+    //用于匹配配置文件中jwt属性下的所有属性
+    public static final String JWT_PREFIX = "jwt";
+
+    //属性名称默认由配置文件中的名称去掉"-"并将"-"后字母大写然后连接,这里配置的属性值应该是属性的默认值
+    //配置属性后需要提供get,set方法,通过commond+N键可以快速创建
+    private String header = "Authorization";
+
+    private String secret = "defaultSecret";
+
+    private Long expiration = 604800L;
+
+    private String authPath = "auth";
+
+    private String md5Key = "randomKey";
+
+    private String ignoreUrl = "";
+
+    public String getIgnoreUrl() {
+        return ignoreUrl;
+    }
+
+    public void setIgnoreUrl(String ignoreUrl) {
+        this.ignoreUrl = ignoreUrl;
+    }
+
+    public static String getJwtPrefix() {
+        return JWT_PREFIX;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public Long getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(Long expiration) {
+        this.expiration = expiration;
+    }
+
+    public String getAuthPath() {
+        return authPath;
+    }
+
+    public void setAuthPath(String authPath) {
+        this.authPath = authPath;
+    }
+
+    public String getMd5Key() {
+        return md5Key;
+    }
+
+    public void setMd5Key(String md5Key) {
+        this.md5Key = md5Key;
+    }
+}
