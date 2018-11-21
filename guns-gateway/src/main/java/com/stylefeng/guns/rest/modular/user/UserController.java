534ea1895473672e469f55c4f72e4088a8d6a8ba
BlackCat <wengyu188@163.com>
2018-11-21 22:09:57 +0800

    UserController is really right complete

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/user/UserController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/user/UserController.java
@@ -4,7 +4,9 @@ package com.stylefeng.guns.rest.modular.user;/**
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.stylefeng.guns.api.user.UserAPI;
+import com.stylefeng.guns.api.user.UserInfoModel;
 import com.stylefeng.guns.api.user.UserModel;
+import com.stylefeng.guns.rest.common.CurrentUser;
 import com.stylefeng.guns.rest.modular.vo.ResponseVO;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -52,6 +54,47 @@ public class UserController {
             }
         }else{
             return ResponseVO.serviceFail("用户名不能为空");
+        }
+
+    }
+
+    @RequestMapping(value = "getUserInfo", method = RequestMethod.GET)
+    public ResponseVO getUserInfo(){
+        //获取登陆用户ID
+        String userId = CurrentUser.getUserId();
+
+        if(userId != null && userId.trim().length()>0){
+            UserInfoModel userInfo = userAPI.getUserInfo(userId);
+            if(userInfo != null){
+                return ResponseVO.success(userInfo);
+            }else{
+                return ResponseVO.appFail("系统出现异常，请联系管理员");
+            }
+
+        }else{
+            return ResponseVO.serviceFail("查询失败，用户尚未登陆");
+        }
+
+    }
+
+    @RequestMapping(value = "updateUserInfo", method = RequestMethod.POST)
+    public ResponseVO updateUserInfo(UserInfoModel userInfoModel) {
+        //获取登陆用户ID
+        String userId = CurrentUser.getUserId();
+
+        if(userId != null && userId.trim().length()>0){
+            if (!userId.equals(userInfoModel.getUuid())) {
+                return ResponseVO.serviceFail("修改信息与登陆信息不符");
+            }
+            UserInfoModel userInfo = userAPI.updateUserInfo(userInfoModel);
+            if(userInfo != null){
+                return ResponseVO.success(userInfo);
+            }else{
+                return ResponseVO.appFail("用户信息修改失败");
+            }
+
+        }else{
+            return ResponseVO.serviceFail("用户尚未登陆");
         }
 
     }
